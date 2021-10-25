2b54c8623e5d1a09495c9dc6a8907f2001a59ea2
AM1897 <89070765+AM1897@users.noreply.github.com>
2021-10-25 22:14:02 +0200

    commit
    Jag har lagt in mina albums i js fil och lagt en kund korg med onclick

---
+++ assignment/inlamning/programmering1/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+<
+js/script.js,4\d\4da5ceae6496884a1f0713d787ace4de5a398774
+<
+js/albums.js,c\4\c4ef1353bbca47378e1595cb4fa92fee10494796
+=
+css/style.css,1\2\126b0f8c1ec95fb3a2541745bfc21365b438d668
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
---
+++ assignment/inlamning/programmering1/js/albums.js
@@ -0,0 +1,63 @@
+let albums = [
+    {
+        image: "img/Skärmbild 2021-10-23 152138.png",
+        name: "Revival 2017",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 152109.png",
+        name: "Relapse 2009",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 152045.png",
+        name: "Music To Be Murdered By 2020",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 152019.png",
+        name: "kamikaze 2018",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 151952.png",
+        name: "Encore 2004",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 151924.png",
+        name: "Curtain Call 2005",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 151843.png",
+        name: "The Eminem Show 2002",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 151722.png",
+        name: "Recovery 2010",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 151625.png",
+        name: "The Marshall Mathers LP 2000",
+        type: "CD",
+        price: 99
+    },
+    {
+        image: "img/Skärmbild 2021-10-23 164100.png",
+        name: "The Marshall Mathers LP 2",
+        type: "CD",
+        price: 99
+    },
+
+]
