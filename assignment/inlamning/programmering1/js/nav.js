baf11dd013bfd363e1a1609905107bb716f2929f
AM1897 <Aram.10@hotmail.com>
2021-10-26 21:55:14 +0200

    commit

---
+++ assignment/inlamning/programmering1/js/nav.js
@@ -0,0 +1,10 @@
+<section className="flex-container">
+    <ul>
+        <li>Våra album</li>
+        <li>Nyheter</li>
+        <li>Topplistor</li>
+        <li>Play</li>
+        <li>Julklappar</li>
+        <li>Erbjudande</li>
+    </ul>
+</section>
